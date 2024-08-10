2bbcc46a11f817d0284a9411015f684b6b0dd586
Samyar <samyar1380@gmail.com>
2024-08-10 23:25:20 +0330

    Test

--- main.py
+++ main.py
@@ -14,32 +14,29 @@ async def start(update:Update, context:ContextTypes.DEFAULT_TYPE):
 
 
 async def rec_con(update:Update, context:ContextTypes.DEFAULT_TYPE):
-    # try:
-    #     user_dir = f"./user_db/{update.effective_user.id}/"
-    #     file_id = update.effective_message.effective_attachment.file_id
-    #     file = await context.bot.get_file(file_id=file_id)
-    #     os.makedirs(user_dir, exist_ok=True)
-    #     await file.download_to_drive(f"{user_dir}document.docx")
-    #     await context.bot.send_message(
-    #         chat_id=update.effective_chat.id,
-    #         text="File is being processed..."
-    #     )
+    try:
+        user_dir = f"./user_db/{update.effective_user.id}/"
+        file_id = update.effective_message.effective_attachment.file_id
+        file = await context.bot.get_file(file_id=file_id)
+        os.makedirs(user_dir, exist_ok=True)
+        await file.download_to_drive(f"{user_dir}document.docx")
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text="File is being processed..."
+        )
 
-    #     convert(f"{user_dir}document.docx", f"{user_dir}document.pdf")
+        convert(f"{user_dir}document.docx", f"{user_dir}document.pdf")
 
-    #     await context.bot.send_document(
-    #         chat_id=update.effective_chat.id,
-    #         document=f"{user_dir}document.pdf")
-    # except:
-    #     await context.bot.send_message(
-    #         chat_id=update.effective_chat.id,
-    #         document=f"An error occured while converting your file, try again.")
+        await context.bot.send_document(
+            chat_id=update.effective_chat.id,
+            document=f"{user_dir}document.pdf")
+    except:
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            document=f"An error occured while converting your file, try again.")
 
-    # if os.path.isdir(user_dir):
-    #     shutil.rmtree(user_dir)
-    await context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        document=f"An error occured while converting your file, try again.")
+    if os.path.isdir(user_dir):
+        shutil.rmtree(user_dir)
     
 
 def main():
