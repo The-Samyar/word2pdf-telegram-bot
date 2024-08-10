8e6a8a1363697d3fbac34dd1e5e822e51f700612
Samyar <samyar1380@gmail.com>
2024-08-10 23:02:33 +0330

    Conversion
    - converts .docx file sent by user to .pdf using docx2pdf

--- constants.py
+++ constants.py
@@ -1,5 +1,5 @@
 # Telegram token
-TOKEN = ""
+TOKEN = "7101820157:AAHGgMulSVKC7X1BjzJH7bvu6Grol59RQeA"
 
 # API Key from https://cloudconvert.com/
 API_KEY = "api_production_e80aa9c90b4069569e42f4cf28b48be7cc9feb1009077c9206150efb903fc52a.66b3dec5377486dc6a45fa4e.66b3df0b9042ac4693a7bc3e"
--- main.py
+++ main.py
@@ -1,20 +1,53 @@
-from telegram import Update
-from telegram.ext import ContextTypes
+from telegram import Update, File
+from telegram.ext import ContextTypes, Application, MessageHandler, filters, CommandHandler
 from constants import *
 from pprint import pprint
+from docx2pdf import convert
 import requests
-ContextTypes
+import os, shutil
 
 async def start(update:Update, context:ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
-        text="Yo yo",
+        text="Send me a file. Make sure it ends with '.docx' .",
     )
 
-async def file_message(update:Update, context:ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text="Send the file. Make sure it ends with '.docx' ."
-    )
 
-async def convert(update:Update, context:ContextTypes): ...
+async def rec_con(update:Update, context:ContextTypes.DEFAULT_TYPE):
+    try:
+        user_dir = f"./user_db/{update.effective_user.id}/"
+        file_id = update.effective_message.effective_attachment.file_id
+        file = await context.bot.get_file(file_id=file_id)
+        os.makedirs(user_dir, exist_ok=True)
+        await file.download_to_drive(f"{user_dir}document.docx")
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text="File is being processed..."
+        )
+
+        convert(f"{user_dir}document.docx", f"{user_dir}document.pdf")
+
+        await context.bot.send_document(
+            chat_id=update.effective_chat.id,
+            document=f"{user_dir}document.pdf")
+    except:
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            document=f"An error occured while converting your file, try again.")
+
+    if os.path.isdir(user_dir):
+        shutil.rmtree(user_dir)
+    
+
+def main():
+    app = Application.builder().token(TOKEN).build()
+
+    app.add_handlers([
+        CommandHandler('start', start),
+        MessageHandler(filters.Document.DOCX, rec_con)
+    ])
+
+    app.run_polling()
+
+if __name__ == '__main__':
+    main()
